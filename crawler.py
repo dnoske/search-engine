70ea334ae840affc533714a2edc8bcf0b334723b
dnoske <dnoske@uni-osnabrueck.com>
2024-12-03 15:50:16 +0100

    fixed some issues

--- crawler.py
+++ crawler.py
@@ -1,30 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-prefix = 'https://vm009.rz.uos.de/crawl/'
+def crawl(prefix, home):
+    """will search for all links that can be accessed by the page defined by home of type html with the same prefix and return an in-memory index""" 
+    start_url = prefix + home
+    agenda = [start_url]
+    visited = {start_url}
+    indexing = {}
 
-start_url = prefix+'index.html'
 
-agenda = [start_url]
-visited = {start_url}
+    while agenda:
+        url = agenda.pop()
+        r = requests.get(url)
+        type = r.headers["Content-Type"]
+        if ("text/html" in type or "application/xhtml+xml" in type) and r.status_code != 404 and r.status_code != 414:
+            if r.status_code != 200:
+                print(r.status_code)
+                print(f"Something went wrong for {url}")
+            print("Process ", url)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            #print(soup)
+            #print()
+            links = soup.find_all('a', href=True)
+            for l in links:
+                link = l["href"]
+                global_link = False
+                if link.startswith(('http://', 'https://', 'mailto:')):
+                    global_link = True
+                if global_link and link[0:len(prefix)] == prefix and not l["href"] in visited:
+                    visited.add(link)
+                    agenda.append(link)
+                elif not global_link:
+                    if link[0] == "/":
+                        link = link[1:]
+                    if not prefix+link in visited:
+                        visited.add(prefix+link)
+                        agenda.append(prefix+link)
 
-while agenda:
-    url = agenda.pop()
-    r = requests.get(url)
-    type = r.headers["Content-Type"]
-    if "text/html" in type or "application/xhtml+xml" in type:
-        print("Process ", url)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        #print(soup)
-        links = soup.find_all('a')
-        for l in links:
-            link = l["href"]
-            global_link = False
-            if link[0:4] == "http":
-                global_link = True
-            if global_link and link[0:len(prefix)] == prefix and not l["href"] in visited:
-                visited.add(l["href"])
-                agenda.append(l["href"])
-            elif not global_link and not prefix+l["href"] in visited:
-                visited.add(prefix+l["href"])
-                agenda.append(prefix+l["href"])
+if __name__ == "__main__":
+    crawl('https://vm009.rz.uos.de/crawl/', 'index.html')
+    #crawl('https://de.wikipedia.org/', 'wiki/Wikipedia:Hauptseite')
+    #crawl('https://www.uni-osnabrueck.de/', 'startseite/')
