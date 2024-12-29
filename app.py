2862371f8575af4cefef7d4c6644fe7572eb9747
sniemann <sascha.niemann66@gmx.de>
2024-12-29 16:30:07 +0100

    styling

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from flask import Flask, request, render_template, redirect, url_for
+"""Search engine as a Flask app."""
+
+from flask import Flask, request, render_template, redirect, url_for, Response
 from crawler import extended_search
 from whoosh.index import open_dir
-import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return redirect(url_for('home'))
+
+@app.route("/")
+def index() -> Response:
+    """Define index route and redirect to /home.
+
+    Returns:
+        Response: redirect to /home
+
+    """
+    return redirect(url_for("home"))
+
 
 @app.route("/home")
 def home():
-    return render_template('start.html')
+    """Process home page request by returning the home page template.
+
+    Returns:
+        str: rendered home page
+
+    """
+    return render_template("start.html")
+
 
 @app.route("/search")
 def search():
+    """Look for the query in the index and return its results.
+
+    Returns:
+        str: rendered html page
+
+    """
     ix = open_dir("indexdir")
-    w = request.args['q']
+    w = request.args["q"]
     r = extended_search(w, ix)
     print(f"{len(r)} elements found")
     for res in r:
         print(r)
-    return render_template('search.html', results=r)
+    return render_template("search.html", results=r, q=w)
