0c270bd1e9bb46aa46046b14b04548cbc9f4d9e5
sniemann <sascha.niemann66@gmx.de>
2024-12-31 12:18:56 +0100

    add highlighted excerpts to search

--- searcher.py
+++ searcher.py
@@ -2,6 +2,7 @@
 
 from whoosh.qparser import QueryParser
 from whoosh.index import open_dir
+from whoosh.highlight import UppercaseFormatter
 from typing import List, Any, Tuple
 
 
@@ -16,32 +17,16 @@ def extended_search(words: str, ix: Any) -> List[Tuple[str, str]]:
         List[Tuple[str, str]]: hits, list of (link, title) tuples
 
     """
-    w = words.split()
-    query_string = " AND ".join(w)
     with ix.searcher() as searcher:
-        query = QueryParser("content", ix.schema).parse(query_string)
+        query = QueryParser("content", ix.schema).parse(words)
         results = searcher.search(query)
+        results.formatter = UppercaseFormatter()
+        results.fragmenter.maxchars = 300
+        results.fragmenter.surround = 50
         hits = []
-        for r in results:
-            hits.append((r["link"], r["title"]))
+        for hit in results:
+            hits.append((hit["link"], hit["title"], hit.highlights("content", top=1)))
         return hits
-
-
-def search(words: str, index: dict) -> List[str]:
-    """Return all links incuding the words from list words using an index returned by crawl().
-
-    Args:
-        words (str): words to search for
-        index (dict): indexing dict
-
-    Returns:
-        List[str]: urls
-
-    """
-    links = index[words[0].lower()]
-    for i in range(1, len(words)):
-        links = links.intersection(index[words[i].lower()])
-    return links
 
 
 if __name__ == "__main__":
